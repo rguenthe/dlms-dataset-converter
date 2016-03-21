4d97d60f7133715fa486ab703ada297ed440e824
Richard Guenther <richard.guenther@web.de>
2016-03-21 18:02:14 +0100

    setup.py: Renamed project to 'dlm-dataset-converter' and added numpy dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='dataset2json',
+    name='dlm-dataset-converter',
     version='1.0',
     packages=[''],
     url='',
@@ -9,5 +9,8 @@ setup(
     author='Richard Guenther',
     author_email='richard.guenther@haw-hamburg.de',
     description='Converts a zip dataset to a JSON dataset',
-    scripts=['dataset2json']
+    scripts=['dsc'],
+	install_requires=[
+		'numpy',
+	],
 )
